26db92e8d501975e664f60835653f5a9a5ed0123
Pradeep-Elumalai <pradeep.e@aciesglobal.com>
2026-06-10 16:09:00 +0530

    [PE] Added a broken version

--- objects/ContainerSummary.py
+++ objects/ContainerSummary.py
@@ -8,10 +8,10 @@ from utils.PydanticBaseModel import PydanticBaseModel
 
 
 class ContainerSummary(PydanticBaseModel):
-    shipmentId: str = Field(description='Shipment ID. Example: "SHIPMENT_1001",')
-    routeId: str = Field(description='Route ID. Example: "ROUTE_CHI_NY",')
-    origin: str = Field(description='Origin location. Example: "Chicago",')
-    destinationInSequence: List[str] = Field(description='Delivery Destinations in sequence. Example: ["New York"],')
+    shipmentId: str = Field(default='', description='Shipment ID. Example: "SHIPMENT_1001",')
+    routeId: str = Field(default='', description='Route ID. Example: "ROUTE_CHI_NY",')
+    origin: str = Field(default='', description='Origin location. Example: "Chicago",')
+    destinationInSequence: List[str] = Field(default_factory=list, description='Delivery Destinations in sequence. Example: ["New York"],')
     totalPallets: float = Field(default=0, description='Total pallets loaded in the container. Example: 6')
     totalWeight: float = Field(default=0, description='Total weight of units loaded in kg (kilogram). Example: 11200')
     totalVolume: float = Field(default=0, description='Total volume occupancy in {m^3} (meter cubic). Example: 864')
