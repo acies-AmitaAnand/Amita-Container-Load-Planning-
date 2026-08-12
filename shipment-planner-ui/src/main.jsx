69bb5afc52fb2a1794a77d76c2db2508542fe21e
Amita Anand <amita.anand@acies.global>
2026-08-12 16:39:10 +0530

    fix: Use HashRouter to support standalone file:// protocol opening

--- shipment-planner-ui/src/main.jsx
+++ shipment-planner-ui/src/main.jsx
@@ -1,10 +1,6 @@
 import React from "react";
-
 import ReactDOM from "react-dom/client";
-
-import {
-  BrowserRouter
-} from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 import "./index.css";
 import "./App.css";
@@ -14,14 +10,9 @@ import App from "./app/App";
 ReactDOM.createRoot(
   document.getElementById("root")
 ).render(
-
   <React.StrictMode>
-
-    <BrowserRouter>
-
+    <HashRouter>
       <App />
-
-    </BrowserRouter>
-
+    </HashRouter>
   </React.StrictMode>
 )
